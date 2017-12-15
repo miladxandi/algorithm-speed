67ab6036ce673f372600b6ed8b4f6edddd40eddd
miladxandi <info@miladzandi.ir>
2017-12-15 15:54:58 +0330

    BubbleSort

--- Algorithms/MainWindow.xaml.cs
+++ Algorithms/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using AccessConnector;
+
 namespace Algorithms
 {
     /// <summary>
@@ -41,25 +42,43 @@ namespace Algorithms
         private void firstBtn_Click(object sender, RoutedEventArgs e)
         {
             Random random = new Random();
-            /* List<int> ID = new List<int> { random.Next(0, int.Parse(txtBox.Text) * 100) };*/
-            int[] myID = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50 };
+
+            int rand = random.Next(1, 80);
+
+            int[] myArray = Enumerable.Range(1, 50).ToArray();
+
+            int[] myRandomArray = new int[50];
+
+            for (int i = 0; i < myRandomArray.Length; i++)
+            {
+                myRandomArray[i] = random.Next(1,80);
+            }
+           
             if (binary.IsChecked == true)
             {
-                Binary(max: myID.Length, inputArray: myID, key: random.Next(1, 80), min: 1);
+                Binary(max: myArray.Length, inputArray: myArray, key: rand, min: 1);
             }
+
             else if (linear.IsChecked == true)
             {
-                Linear(max: myID.Length, inputArray: myID, key: random.Next(1, 80), min: 1);
+                Linear(max: myArray.Length, inputArray: myArray, key: rand, min: 1);
             }
+
             else if (qsort.IsChecked == true)
             {
+                QuickSort(max: myRandomArray.Length, inputArray: myRandomArray, key: rand, min: 1);
             }
+
             else if (bsort.IsChecked == true)
             {
+                BubbleSort(max: myRandomArray.Length, inputArray: myRandomArray, min: 1);
             }
+
             else if (hanoi.IsChecked == true)
             {
+
             }
+
             else
             {
                 Color Red = new Color();
@@ -151,6 +170,90 @@ namespace Algorithms
                 Red = Color.FromRgb(184, 35, 35);
                 lblText.Foreground = new SolidColorBrush(Red);
                 lblText.Text = $"Nah! We cant find {key} .";
+                return "Nah! the value not found";
+            }
+            catch (Exception error)
+            {
+                Color Red = new Color();
+                Red = Color.FromRgb(184, 35, 35);
+                lblText.Foreground = new SolidColorBrush(Red);
+                lblText.Text = $"Nah! The {error.Message} value did not found";
+                return $"Nah! The {error.Message} value did not found";
+            }
+        }
+        public object QuickSort(int[] inputArray, int key, int max, int min)
+        {
+            try
+            {
+                var sTime = DateTime.Now.Millisecond;
+                for (int k = min; k < max; k++)
+                {
+                    System.Threading.Thread.Sleep(1);
+
+                    if (inputArray[k] == key)
+                    {
+                        Color Green = new Color();
+                        Green = Color.FromRgb(32, 238, 0);
+                        lblText.Foreground = new SolidColorBrush(Green);
+                        lblText.Text = $"Yeah! We`ve found {key} .";
+                        var fTime = DateTime.Now.Millisecond;
+                        int Time = fTime - sTime;
+                        liView.Items.Add($"The generated value was {key} & founded in {Time} millisecond with Linear search.");
+                        //Connect.ConnectionOpener(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\farva\Source\Repos\Algorithms\Algorithms\Assets\Algorithms.accdb;Persist Security Info=True", "INSERT INTO BinaryLog ([Time], [Number]) VALUES('" + int.Parse(Time.ToString()) + "','" + int.Parse(key.ToString()) + "')");
+                        return k;
+                    }
+
+                }
+                return "Nah! the value not found";
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Color Red = new Color();
+                Red = Color.FromRgb(184, 35, 35);
+                lblText.Foreground = new SolidColorBrush(Red);
+                lblText.Text = $"Nah! We cant find {key} .";
+                return "Nah! the value not found";
+            }
+            catch (Exception error)
+            {
+                Color Red = new Color();
+                Red = Color.FromRgb(184, 35, 35);
+                lblText.Foreground = new SolidColorBrush(Red);
+                lblText.Text = $"Nah! The {error.Message} value did not found";
+                return $"Nah! The {error.Message} value did not found";
+            }
+        }
+        public object BubbleSort(int[] inputArray, int max, int min)
+        {
+            try
+            {
+                var sTime = DateTime.Now.Millisecond;
+                int t;
+                for (int p = 0; p <= inputArray.Length - 2; p++)
+                {
+                    System.Threading.Thread.Sleep(1);
+
+                    for (int i = 0; i <= inputArray.Length - 2; i++)
+                    {
+                        if (inputArray[i] > inputArray[i + 1])
+                        {
+                            t = inputArray[i + 1];
+                            inputArray[i + 1] = inputArray[i];
+                            inputArray[i] = t;
+                        }
+                    }
+                }
+                Color Green = new Color();
+                Green = Color.FromRgb(32, 238, 0);
+                lblText.Foreground = new SolidColorBrush(Green);
+                lblText.Text = $"Yeah! We`ve sorted {inputArray} .";
+                var fTime = DateTime.Now.Millisecond;
+                int Time = fTime - sTime;
+                liView.Items.Add($"The array were sorted in {Time} millisecond with Bubble sort with these values:");
+                foreach (var item in inputArray)
+                {
+                    liView.Items.Add($"{item}");
+                }
                 return "Nah! the value not found";
             }
             catch (Exception error)
